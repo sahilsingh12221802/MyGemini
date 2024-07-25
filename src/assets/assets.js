d951c916ea98517d4aaa49d9abaa7f1ea4745321
sahil singh <hisahilsingh730@gmail.com>
2024-07-25 19:04:29 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,35 @@
+import history_icon from './history_icon.svg'
+import menu_icon from './menu_icon.svg'
+import plus_icon from './plus_icon.svg'
+import question_icon from './question_icon.svg'
+import setting_icon from './setting_icon.svg'
+import bulb_icon from './bulb_icon.svg'
+import gallery_icon from './gallery_icon.svg'
+import mic_icon from './mic_icon.svg'
+import user_icon from './user_icon.png'
+import youtube_icon from './youtube_icon.png'
+import message_icon from './message_icon.svg'
+import code_icon from './code_icon.svg'
+import send_icon from './send_icon.svg'
+import gemini_icon from './gemini_icon.png'
+import trips_icon from './trips_icon.svg'
+import puzzle_icon from './puzzle_icon.svg'
+
+export const assets = {
+    history_icon,
+    menu_icon,
+    plus_icon,
+    question_icon,
+    setting_icon,
+    bulb_icon,
+    gallery_icon,
+    mic_icon,
+    user_icon,
+    youtube_icon,
+    message_icon,
+    code_icon,
+    send_icon,
+    trips_icon,
+    puzzle_icon,
+    gemini_icon
+}
